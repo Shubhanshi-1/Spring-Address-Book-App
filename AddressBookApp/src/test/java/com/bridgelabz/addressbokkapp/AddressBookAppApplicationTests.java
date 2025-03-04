d8b321fbf6abd921a01f39afe83fe5f5fe8479ce
Shubhanshi <vermashashi3421@gmail.com>
2025-03-04 10:29:40 +0530

    [Shubhanshi]Add. Created string starter project for Address Book app

---
+++ AddressBookApp/src/test/java/com/bridgelabz/addressbokkapp/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbokkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
